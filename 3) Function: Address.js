3727cffbb275d15fc103a7461354edf34ae0bf7a
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-07-16 14:33:48 +0530

    Create 3) Function: Address.js

---
+++ 3) Function: Address.js
@@ -0,0 +1,7 @@
+function Hello(age, address) 
+{
+  var name=prompt("What is your name");
+  alert("Hello "+name+"!"+" Your age is: "+age+" & Your address is: "+address)  
+}
+
+Hello(24, "Mumbai");
