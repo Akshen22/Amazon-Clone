e7713f9694b5c3258e99a61d5b6b54dfa4fc2d66
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-07-16 14:50:27 +0530

    Update 4) Return.js

--- 4) Return.js
+++ 4) Return.js
@@ -1,7 +1,7 @@
 #previously we couldn't see the output in the console, it used to be: undefined
 #so, I will use return!
 
-function Hello(age, address) 
+a) function Hello(age, address) 
 {
   var name=prompt("What is your name");
   age=age/1.5;
@@ -10,3 +10,7 @@ function Hello(age, address)
 }
 
 Hello(24, "Mumbai");
+
+b) var: saves data!
+var dogyears= Hello(24,"Mumbai")
+#whenever will write dog-years, will get answer 16!
